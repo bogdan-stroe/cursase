62d3f3e0c30a138901b8da1c7e21ec81820102f0
bogdan-stroe <bogdan-stroe@users.noreply.github.com>
2016-04-05 12:07:03 +0000

    Create ShoppingCartServiceImpl.java

---
+++ ShoppingCartServiceImpl.java
@@ -0,0 +1,39 @@
+package com.oracle.shopping.service;
+
+import java.util.Map;
+
+import com.oracle.shopping.model.Item;
+
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+	@Override
+	public void addItem(Item newItem, int quantity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateItem(Item item, int quantity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeItem(Item item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int computePrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Map<Item, Integer> getShoppingCart() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
